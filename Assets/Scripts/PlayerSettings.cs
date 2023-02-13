2bfc2eadc4a69fb7bfdad8fa63e6cf60e4ec3637
Elliot <93895160+2Elliot@users.noreply.github.com>
2023-02-12 23:33:32 -0800

    Continue working on settings menu

--- Assets/Scripts/PlayerSettings.cs
+++ Assets/Scripts/PlayerSettings.cs
@@ -1,14 +1,19 @@
 using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerSettings : MonoBehaviour
 {
 
+	[SerializeField] GameObject postProcessing;
 	[SerializeField] private AudioSource[] audioSource;
 
+	private Bloom bloom;
+
 	private void Start() {
+		postProcessing.GetComponent<Volume>().profile.TryGet(out bloom);
+
 		ApplySettings();
 	}
 
@@ -19,5 +24,11 @@ public class PlayerSettings : MonoBehaviour
 		foreach (AudioSource audio in audioSource) {
 			audio.volume = PlayerPrefs.GetFloat("Audio_Master") * PlayerPrefs.GetFloat("Audio_Effects");
 		}
+
+		Screen.brightness = (float)(PlayerPrefs.GetFloat("Video_Brightness") * 1.5);
+		bloom.intensity.value = PlayerPrefs.GetFloat("Video_Bloom") * 3;
+		Debug.Log(Screen.brightness);
+		Debug.Log((float)(PlayerPrefs.GetFloat("Video_Brightness") * 1.5));
+
 	}
 }
