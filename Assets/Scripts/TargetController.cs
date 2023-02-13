64ba247fdb00a235cc23539aa54a5ee22ce9b941
DoomKitty87 <doomkitty887@gmail.com>
2023-02-13 13:34:26 -0800

    more responsive targets on hit

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -12,4 +12,26 @@ public class TargetController : MonoBehaviour
     {
         health = targetHealth[(int)float.Parse(gameObject.name.Substring(6, 1)) - 1];
     }
+
+    public void IsHit() {
+        StopCoroutine("Bounce");
+        StartCoroutine(Bounce());
+    }
+
+    private IEnumerator Bounce() {
+        Vector3 init = transform.localScale;
+        float timer = 0f;
+        while (timer < 0.15f) {
+            transform.localScale = Vector3.Lerp(init, init * 0.8f, timer / 0.15f);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        timer = 0f;
+        while (timer < 0.25f) {
+            transform.localScale = Vector3.Lerp(init * 0.8f, init, timer / 0.25f);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = init;
+    }
 }
