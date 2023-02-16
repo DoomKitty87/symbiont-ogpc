39c6c23fb0ce723ec9b1993a06c6b84fa662702f
DoomKitty87 <doomkitty887@gmail.com>
2023-02-16 08:58:54 -0800

    Update AccountInterface.cs

--- Assets/Scripts/AccountInterface.cs
+++ Assets/Scripts/AccountInterface.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class AccountInterface : Monobehaviour
+public class AccountInterface : MonoBehaviour
 {
 
   private LoginConnect loginManager;
