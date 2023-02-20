8fa400208297cc709c89381a8ece41a8a44beddc
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2023-02-19 18:34:37 -0800

    refactoring target/object "controllers"

---
+++ Assets/Scripts/Effects/BounceEffect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class BounceEffect : MonoBehaviour
+{
+    public void StartEffect()
+    {
+        StopCoroutine(Bounce());
+        StartCoroutine(Bounce());
+    }
+    private IEnumerator Bounce() {
+        Vector3 init = transform.localScale;
+        float timer = 0f;
+        while (timer < 0.075f) {
+            transform.localScale = Vector3.Lerp(init, init * 0.9f, timer / 0.075f);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        timer = 0f;
+        while (timer < 0.1f) {
+            transform.localScale = Vector3.Lerp(init * 0.9f, init, timer / 0.1f);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = init;
+    }
+}
