a5338243a6726c083f7c0211ab727f84ee1d1452
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2023-02-19 19:10:14 -0800

    update to conform to conventions

--- Assets/Scripts/Health/DisplayHealth.cs
+++ Assets/Scripts/Health/DisplayHealth.cs
@@ -7,78 +7,65 @@ using TMPro;
 
 public class DisplayHealth : MonoBehaviour
 {
-    [Header("UI References")]
-    [SerializeField][Tooltip("This is optional.")] private TextMeshProUGUI _textElement;
-    [SerializeField] private string _textElementPrefix;
-    [SerializeField][Range(0, 10)] private int _healthTextDecimalPlaces;
-    [SerializeField][Tooltip("This is optional.")] private Slider _healthSlider;
+  [Header("UI References")]
+  [SerializeField][Tooltip("This is optional.")] private TextMeshProUGUI _textElement;
+  [SerializeField] private string _textElementPrefix;
+  [SerializeField][Range(0, 10)] private int _healthTextDecimalPlaces;
+  [SerializeField][Tooltip("This is optional.")] private Slider _healthSlider;
 
+  [Header("Tweening Values")]
+  [SerializeField] private AnimationCurve _easeCurve;
+  [SerializeField] private float _easeDuration;
 
-    [Header("Tweening Values")]
-    [SerializeField] private AnimationCurve _easeCurve;
-    [SerializeField] private float _easeDuration;
+  // Debounce
+  private bool _tweeningHealth;
 
-    // Debounce
-    private bool _tweeningHealth;
-
-    private void Start()
-    {
-        if (_textElement == null & _healthSlider == null)
-        {
-            Debug.LogWarning("DisplayHealth: Both UI Elements are null. Are you missing a reference?");
-        }
+  private void Start() {
+    if (_textElement == null & _healthSlider == null) {
+      Debug.LogWarning("DisplayHealth: Both UI Elements are null. Are you missing a reference?");
     }
-    public void OnHealthInitialize(float maxHealth)
-    {
-        if (_textElement != null)
-        {
-            _textElement.text = _textElementPrefix + maxHealth.ToString();
-        }
-        if (_healthSlider != null)
-        {
-            _healthSlider.value = 1;
-        }
+  }
+  public void OnHealthInitialize(float maxHealth) {
+    if (_textElement != null) {
+      _textElement.text = _textElementPrefix + maxHealth.ToString();
     }
-    public void OnHealthChanged(float initHealth, float currentHealth, float maxHealth)
-    {
-        if (_textElement != null)
-        {
-            StartCoroutine(TweenTextValue(_textElement, initHealth, currentHealth, _easeDuration, _healthTextDecimalPlaces));
-        }
-        if (_healthSlider != null)
-        {
-            StartCoroutine(TweenSlider(_healthSlider, currentHealth / maxHealth, _easeDuration));
-        }
+    if (_healthSlider != null) {
+      _healthSlider.value = 1;
     }
-    private IEnumerator TweenSlider(Slider slider, float targetValue, float duration)
-    {
-        float timeElapsed = 0;
-        float initSliderValue = slider.value;
-        while (timeElapsed < duration)
-        {
-            timeElapsed += Time.deltaTime;
-            float t = timeElapsed / duration;
-            t = _easeCurve.Evaluate(t); 
-            slider.value = Mathf.Lerp(initSliderValue, targetValue, t);
-            yield return null;
-        }
-        slider.value = targetValue;
+  }
+  public void OnHealthChanged(float initHealth, float currentHealth, float maxHealth) {
+    if (_textElement != null) {
+      StartCoroutine(TweenTextValue(_textElement, initHealth, currentHealth, _easeDuration, _healthTextDecimalPlaces));
     }
-    private IEnumerator TweenTextValue(TextMeshProUGUI text, float startValue, float targetValue, float duration, int decimalPlaces)
-    {
-        float timeElapsed = 0;
-        // float initSliderValue = slider.value;
-        while (timeElapsed < duration)
-        {
-            timeElapsed += Time.deltaTime;
-            float t = timeElapsed / duration;
-            t = _easeCurve.Evaluate(t); 
-            text.text = _textElementPrefix + Math.Round(Mathf.Lerp(startValue, targetValue, t), decimalPlaces).ToString();
-            yield return null;
-        }
-        text.text = _textElementPrefix + targetValue.ToString();
+    if (_healthSlider != null) {
+      StartCoroutine(TweenSlider(_healthSlider, currentHealth / maxHealth, _easeDuration));
     }
+  }
+  private IEnumerator TweenSlider(Slider slider, float targetValue, float duration) {
+    float timeElapsed = 0;
+    float initSliderValue = slider.value;
+    while (timeElapsed < duration) {
+      timeElapsed += Time.deltaTime;
+      float t = timeElapsed / duration;
+      t = _easeCurve.Evaluate(t);
+      slider.value = Mathf.Lerp(initSliderValue, targetValue, t);
+      yield return null;
+    }
+    slider.value = targetValue;
+  }
+  private IEnumerator TweenTextValue(TextMeshProUGUI text, float startValue, float targetValue, float duration, int decimalPlaces) {
+    float timeElapsed = 0;
+    // float initSliderValue = slider.value;
+    while (timeElapsed < duration) {
+      timeElapsed += Time.deltaTime;
+      float t = timeElapsed / duration;
+      t = _easeCurve.Evaluate(t);
+      text.text = _textElementPrefix + Math.Round(Mathf.Lerp(startValue, targetValue, t), decimalPlaces).ToString();
+      yield return null;
+    }
+    text.text = _textElementPrefix + targetValue.ToString();
+  }
 }
 
-    
+
 
--- Assets/Scripts/Health/HealthManager.cs
+++ Assets/Scripts/Health/HealthManager.cs
@@ -9,33 +9,29 @@ public class OnHealthInitializeEvent : UnityEvent<float> {}
 public class OnHealthChangedEvent : UnityEvent<float, float, float> {}
 public class HealthManager : MonoBehaviour
 {
-    public float _currentHealth;
-    [SerializeField] private float _maxHealth;
-    [Tooltip("OnHealthInitialize(_maxHealth)")] public OnHealthInitializeEvent _onHealthInitialize;
-    [Tooltip("OnHealthZero()")] public UnityEvent _onHealthZero;
-    [Tooltip("OnHealthChanged(healthBeforeDamage, _currentHealth, _maxHealth)")] public OnHealthChangedEvent _onHealthChanged;
+  public float _currentHealth;
+  [SerializeField] private float _maxHealth;
+  [Tooltip("OnHealthInitialize(_maxHealth)")] public OnHealthInitializeEvent _onHealthInitialize;
+  [Tooltip("OnHealthZero()")] public UnityEvent _onHealthZero;
+  [Tooltip("OnHealthChanged(healthBeforeDamage, _currentHealth, _maxHealth)")] public OnHealthChangedEvent _onHealthChanged;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _currentHealth = _maxHealth;
-        _onHealthInitialize?.Invoke(_maxHealth);
+  // Start is called before the first frame update
+  void Start() {
+    _currentHealth = _maxHealth;
+    _onHealthInitialize?.Invoke(_maxHealth);
+  }
+  public void Damage(float damagePoints) {
+    if (_currentHealth - damagePoints <= 0) {
+      _onHealthZero?.Invoke();
+      float initialHealth = _currentHealth;
+      _currentHealth -= damagePoints;
+      _onHealthChanged?.Invoke(initialHealth, _currentHealth, _maxHealth);
+      this.enabled = false;
     }
-    public void Damage(float damagePoints)
-    {
-        if (_currentHealth - damagePoints <= 0)
-        {
-            _onHealthZero?.Invoke();
-            float initialHealth = _currentHealth;
-            _currentHealth -= damagePoints;
-            _onHealthChanged?.Invoke(initialHealth, _currentHealth, _maxHealth);
-            this.enabled = false;
-        }
-        else
-        {
-            float initialHealth = _currentHealth;
-            _currentHealth -= damagePoints;
-            _onHealthChanged?.Invoke(initialHealth, _currentHealth, _maxHealth);
-        }
+    else {
+      float initialHealth = _currentHealth;
+      _currentHealth -= damagePoints;
+      _onHealthChanged?.Invoke(initialHealth, _currentHealth, _maxHealth);
     }
+  }
 }
