5d02f0e1c60ef252dc451b502c97ed407416b83b
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2023-02-20 11:57:24 -0800

    update menuShowHide to use style conventions

--- Assets/Scripts/UI Menus/MenuShowHide.cs
+++ Assets/Scripts/UI Menus/MenuShowHide.cs
@@ -11,63 +11,63 @@ public class MenuShowHide : MonoBehaviour
   [SerializeField] private CanvasGroup _canvasGroup;
   [SerializeField] private float _fadeTime;
 
-    // Start is called before the first frame update
-    private void Start() {
-        if (_menuElements.Count == 0) {
-          Debug.LogError("MenuShowHide: Menu Elements are null!", gameObject);
-          this.enabled = false;
-        }
-        if (_fading && _canvasGroup == null)
-        {
-          Debug.LogError("MenuShowHide: Canvas Group is null!", gameObject);
-          this.enabled = false;
-        }
+  // Start is called before the first frame update
+  private void Start() {
+    if (_menuElements.Count == 0) {
+      Debug.LogError("MenuShowHide: Menu Elements are null!", gameObject);
+      this.enabled = false;
     }
-    public void Show() {
-      if (_fading) {
-        foreach (GameObject element in _menuElements) {
-          element.SetActive(true);
-        }
-        StartCoroutine(ShowCanvasGroup());
-      }
-      else {
-        foreach (GameObject element in _menuElements) {
-          element.SetActive(true);
-        }
-      }
-    }
-    public void Hide() {
-      if (_fading) {
-        foreach (GameObject element in _menuElements) {
-          element.SetActive(false);
-        }
-        StartCoroutine(HideCanvasGroup());
-      }
-      else {
-        foreach (GameObject element in _menuElements) {
-          element.SetActive(false);
-        }
-      }
-    }
-    private IEnumerator ShowCanvasGroup()
+    if (_fading && _canvasGroup == null)
     {
-      float timeElapsed = 0;
-      while (timeElapsed < _fadeTime) {
-        timeElapsed += Time.deltaTime;
-        _canvasGroup.alpha = timeElapsed / _fadeTime;
-        yield return null;
+      Debug.LogError("MenuShowHide: Canvas Group is null!", gameObject);
+      this.enabled = false;
+    }
+  }
+  public void Show() {
+    if (_fading) {
+      foreach (GameObject element in _menuElements) {
+        element.SetActive(true);
       }
-      _canvasGroup.alpha = 1;
-    } 
-    private IEnumerator HideCanvasGroup()
-    {
-      float timeElapsed = 0;
-      while (timeElapsed < _fadeTime) {
-        timeElapsed += Time.deltaTime;
-        _canvasGroup.alpha = 1 - (timeElapsed / _fadeTime);
-        yield return null;
+      StartCoroutine(ShowCanvasGroup());
+    }
+    else {
+      foreach (GameObject element in _menuElements) {
+        element.SetActive(true);
       }
-      _canvasGroup.alpha = 0;
-    } 
+    }
+  }
+  public void Hide() {
+    if (_fading) {
+      foreach (GameObject element in _menuElements) {
+        element.SetActive(false);
+      }
+      StartCoroutine(HideCanvasGroup());
+    }
+    else {
+      foreach (GameObject element in _menuElements) {
+        element.SetActive(false);
+      }
+    }
+  }
+  private IEnumerator ShowCanvasGroup()
+  {
+    float timeElapsed = 0;
+    while (timeElapsed < _fadeTime) {
+      timeElapsed += Time.deltaTime;
+      _canvasGroup.alpha = timeElapsed / _fadeTime;
+      yield return null;
+    }
+    _canvasGroup.alpha = 1;
+  } 
+  private IEnumerator HideCanvasGroup()
+  {
+    float timeElapsed = 0;
+    while (timeElapsed < _fadeTime) {
+      timeElapsed += Time.deltaTime;
+      _canvasGroup.alpha = 1 - (timeElapsed / _fadeTime);
+      yield return null;
+    }
+    _canvasGroup.alpha = 0;
+  } 
 }
 
