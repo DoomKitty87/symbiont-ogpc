efa44b2d2160481425725a943be59fc0ba5f44b0
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2023-03-01 21:09:10 -0800

    started refactoring gun script in _ScriptableTest

---
+++ Assets/Scripts/Gun Controller/FireInput.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class FireInput : MonoBehaviour
+{
+  [Header("Input Axis")]
+  [SerializeField][Tooltip("Defaults to 'Fire1' if axis is empty or doesn't exist.")] private string _fireInputAxis;
+  [Tooltip("Fires on the first frame FireInputAxis is 1.")] public UnityEvent _OnFireInputDown;
+  [Tooltip("Fires on every frame FireInputAxis is 1, except for the first frame.")] public UnityEvent _OnFireInputHeld;
+  [Tooltip("Fires on the frame after the last frame FireInputAxis is 1.")] public UnityEvent _OnFireInputUp;
+  private bool _hadInputLastFrame;
+
+  private void Start() {
+    _hadInputLastFrame = false;
+    if (!IsAxisSetup("Fire1")) Debug.LogError("FireInput: Fire1 is not setup! FireInputAxis has no default, and will be unassigned if the axis name is invalid.");
+    if (IsAxisSetup(_fireInputAxis)) return;
+    else _fireInputAxis = "Fire1";
+  }
+
+  private bool IsAxisSetup(string axisName) {
+    try {
+      Input.GetAxis(axisName);
+      return true;
+    }
+    catch {
+      return false;
+    }
+  }
+  
+  private void Update() {
+    if (!_hadInputLastFrame) {
+      if (Input.GetAxisRaw(_fireInputAxis) == 1) {
+        _hadInputLastFrame = true;
+        _OnFireInputDown?.Invoke();
+      }
+      else {
+        _hadInputLastFrame = false;
+      }
+    }
+    else if (_hadInputLastFrame) {
+      if (Input.GetAxisRaw(_fireInputAxis) == 1) {
+        _hadInputLastFrame = true;
+        _OnFireInputHeld?.Invoke();
+      }
+      else {
+        _hadInputLastFrame = false;
+        _OnFireInputUp?.Invoke();
+      }
+    }
+  }
+}
--- Assets/Scripts/Gun Controller/FireRaycast.cs
+++ Assets/Scripts/Gun Controller/FireRaycast.cs
@@ -11,27 +11,35 @@ public class FireRaycast : MonoBehaviour
 
   [Header("Hit Settings")]
   [SerializeField] private float _hitDamage;
-  // Start is called before the first frame update
+  
+  [Header("Debug")]
+  [SerializeField] private bool _debug;
+  
   void Start() {
     if (_fireOrigin == null) {
       Debug.LogError("FireRaycast: FireOrgin is null!");
     }
     if (_fireDistance <= 0) {
-      Debug.LogWarning("FireRaycast: FireDistanve is less than or equal to 0. Is this intended?");
+      Debug.LogWarning("FireRaycast: FireDistance is less than or equal to 0. Is this intended?");
     }
   }
 
-  public Vector3 Fire() {
+  public void Fire() {
+    DrawRaycastLine(Color.green, 1f);
     if (Physics.Raycast(_fireOrigin.position, _fireOrigin.forward, out RaycastHit hit, _fireDistance, _layerMask)) {
       GameObject hitGameObject = hit.collider.gameObject;
       HealthManager healthManager = hitGameObject.GetComponent<HealthManager>();
       if (healthManager != null) {
         healthManager.Damage(_hitDamage);
+        DrawRaycastLine(Color.yellow, 1f);
       }
-      return hit.point;
     }
-    else {
-        return (_fireOrigin.forward * _fireDistance) + _fireOrigin.position;
+  }
+
+  private void DrawRaycastLine(Color lineColor, float durationSeconds)
+  {
+    if (_debug) {
+      Debug.DrawLine(_fireOrigin.position, (_fireOrigin.forward * _fireDistance) + _fireOrigin.position, lineColor, durationSeconds);
     }
   }
 }
