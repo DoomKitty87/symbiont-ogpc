e1cbf2572bcb9623ad9db614d98937e570c6dad4
Elliot <ejdalessa@gmail.com>
2023-03-14 13:52:02 -0700

    Don't press escape in the menu please

--- Assets/_Scripts/UI Managers/PauseHandler.cs
+++ Assets/_Scripts/UI Managers/PauseHandler.cs
@@ -1,61 +1,88 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseHandler : MonoBehaviour 
 {
-	[SerializeField] private GameObject[] objectsToBeShown;
 	[SerializeField] private GameObject[] objectsToBeHidden;
 
-	private ButtonScript buttonScript;
+	public List<string> currentActiveElements;
+
+	public string defaultCurrentActiveElement;
+
+	public ButtonScript buttonScript;
 	private bool isPaused = false;
 
+	private void Awake() {
+		buttonScript = GetComponent<ButtonScript>();
+	}
+
 	private void Start() {
-		UnPause();
+		currentActiveElements.Add(defaultCurrentActiveElement);
+		// UnPause();
 	}
 
 	private void Update() {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
-			if (isPaused) UnPause(); else Pause();
+			if (currentActiveElements.Count == 1) InitPause(); else if (currentActiveElements.Count == 2) Unpause(); else RemovePause();
 		}
 	}
 
-	// Handles hiding and unhiding objects when paused
-	public void Pause() {
-
-		isPaused = true;
+	// Handles when pause key is first pressed
+	public void InitPause() {
 
 		Cursor.lockState = CursorLockMode.None;
 		Cursor.visible = true;
 
-		foreach (GameObject thing in objectsToBeShown) thing.SetActive(true);
 		foreach (GameObject thing in objectsToBeHidden) thing.SetActive(false);
+
+		buttonScript.ButtonChangeMenuScreen("Pause Menu");
+
+		currentActiveElements.Add("Pause Menu");
+
 		Time.timeScale = 0.0f;
 	}
 
-	// Function called from ButtonScript to handle unpausing
-	public void UnPause() {
+	// Adds an object to pause array
+	public void AddPause() {
+
+	}
+
+	// Removes an object from pause array
+	public void RemovePause() {
+
+	}
+
+	// Hanldes when pause fully unpauses menu
+	public void Unpause() {
 
 		isPaused = false;
 
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;		
 
+		// currentActiveElement.SetActive(false);
+
 		foreach (GameObject thing in objectsToBeHidden) thing.SetActive(true);
-		foreach (GameObject thing in objectsToBeShown) thing.SetActive(false);
 		Time.timeScale = 1.0f;
 	}
 
 	// Glorified switch statement
 	private void HandleNestedMenus() {
-		switch(buttonScript.currentActiveElement) {
+		/*switch(currentActiveElement.name) {
+			case "Pause Menu":
+				UnPause();
+				break;
+			case "Settings Menu":
+				buttonScript.ButtonChangeMenuScreen("Pause Menu");
+				break;
 			case null:
-				buttonScript.ButtonHideScreen();
+				Debug.LogError("currentActiveElement null error");
 				break;
 			default:
-				buttonScript.ButtonChangeMenuScreen("Pause Menu");
+				buttonScript.ButtonChangeMenuScreen("Settings Menu");
 				break;
 
 		}
+		*/
 	}
 }
