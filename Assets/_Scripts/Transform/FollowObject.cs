236c4dbadf78bd59f26113155405aee4abe1ccbe
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2023-03-19 10:06:57 -0700

    Auto stash before merge of "experimental" and "origin/experimental"

--- Assets/_Scripts/Transform/FollowObject.cs
+++ Assets/_Scripts/Transform/FollowObject.cs
@@ -11,30 +11,24 @@ public class FollowObject : MonoBehaviour
     [SerializeField] private bool _followZ;
     private Vector3 _startPosition;
     // Start is called before the first frame update
-    void Start()
-    {
-        if (_transformToFollow == null)
-        {
-            Debug.LogError("FollowObject: TransformToFollow is null!");
-        }
-        _startPosition = transform.position;
+    void Start() {
+      if (_transformToFollow == null) {
+        Debug.LogError("FollowObject: TransformToFollow is null!");
+      }
+      _startPosition = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (_followX)
-        {
-            transform.position = new Vector3(_transformToFollow.position.x, transform.position.y, transform.position.z);
-        }
-        if (_followY)
-        {
-            transform.position = new Vector3(transform.position.x, _transformToFollow.position.y, transform.position.z);
-        }
-        if (_followZ)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, _transformToFollow.position.z);
-        }
-        transform.position += _followOffset;
+    void Update() {
+      if (_followX) {
+        transform.position = new Vector3(_transformToFollow.position.x, transform.position.y, transform.position.z);
+      }
+      if (_followY) {
+        transform.position = new Vector3(transform.position.x, _transformToFollow.position.y, transform.position.z);
+      }
+      if (_followZ) {
+        transform.position = new Vector3(transform.position.x, transform.position.y, _transformToFollow.position.z);
+      }
+      transform.position += _followOffset;
     }
 }
