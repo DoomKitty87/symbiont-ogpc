ad17022e9c7208c470f25cc2ce9507d8913b69ab
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2023-03-31 12:58:04 -0700

    Switching finished

---
+++ Assets/_Scripts/Switching/SwitchableObject.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchableObject : MonoBehaviour
+{
+  // This script should contain a camera and an audio listener that are disabled and enabled 
+  // when switching, 
+
+  [Header("Note: To deem this object switchable, its tag will be overridden.")]
+  
+  [Header("Selection Raycast")]
+
+  public Transform _raycastOrigin;
+  public Vector3 _raycastOriginOffset;
+  public float _raycastDistance;
+  public LayerMask _layerMask;
+  public bool _showRaycast;
+
+  [Header("Object References")]
+  [Header("Note: The FOV Effect will only play for the first camera in this list.")] // Could make it so that the list has a selection of whether to use the effect or not, but the KIS rule says no
+  public List<Camera> _objectCameras = new();
+  public AudioListener _objectAudioListener;
+
+  [Header("Events")]
+  public UnityEvent _OnSwitchedTo;
+  public UnityEvent _OnSwitchedAway;  
+  public UnityEvent _OnSelected;
+  public UnityEvent _OnUnselected;
+
+  private void OnValidate() {
+    gameObject.tag = "SwitchableObject";
+  }
+  private void Start() {
+    ChangeStateOfAllCamerasTo(false);
+    _objectAudioListener.enabled = false;
+    _OnSwitchedAway?.Invoke();
+  }
+  public void Selected(bool isSelected) {
+    if (isSelected) {
+      _OnSelected?.Invoke();
+    }
+    else {
+      _OnUnselected?.Invoke();
+    }
+  }
+  public void SwitchTo() {
+    _OnSwitchedTo?.Invoke();
+    ChangeStateOfAllCamerasTo(true);
+    _objectAudioListener.enabled = true;
+  }
+  public void SwitchAway() {
+    _OnSwitchedAway?.Invoke();
+    ChangeStateOfAllCamerasTo(false);
+    _objectAudioListener.enabled = false;
+  }
+  private void ChangeStateOfAllCamerasTo(bool enabled) {
+    for (int i = 0; i < _objectCameras.Count; i++) {
+      _objectCameras[i].enabled = enabled;
+    }
+  }
+}
--- Assets/_Scripts/Switching/ViewSwitcher.cs
+++ Assets/_Scripts/Switching/ViewSwitcher.cs
@@ -2,6 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class SwitchableObjectRegister 
+{
+  public bool _currentlySwitchedTo;
+  public SwitchableObject _switchComponent;
+
+  public SwitchableObjectRegister(bool currentlySwitchedTo, SwitchableObject switchableObject) {
+    _currentlySwitchedTo = currentlySwitchedTo;
+    _switchComponent = switchableObject;
+  }
+}
+
 public class ViewSwitcher : MonoBehaviour
 {
   // On a trigger, activate an effect (tbd, fov increase animation for now) + sounds and change the
@@ -19,21 +31,152 @@ public class ViewSwitcher : MonoBehaviour
   //
   // This should probably also switch off the player input for the current target.
   
-  [Header("Camera")]
-  [Header("This needs to be assigned before runtime to an 'inital' switchable object.")]
-  [SerializeField] private Camera _currentPlayerCamera;
-  [SerializeField] private float _startFov;
-  [Header("Switching Effect")]
-  [SerializeField] private AnimationCurve _effectCurve;  
+  [Header("Objects")]
+  [Header("Assign an object here to start off with when the game loads.")]
+  [SerializeField] private SwitchableObject _currentObjectInhabiting;
+  [SerializeField] private SwitchableObject _selectedSwitchableObject;
+  [Header("Note: Configurations for selection raycasts are on each object")]
+  [Header("Input")]
+  [SerializeField] private string _switchAxis;
+  [SerializeField] private float _secondsSinceLastSwitch = 0f;
+  [SerializeField] private float _switchCooldown;
+  private bool _switchAxisDown = false; // Only true on first frame axis is detected
+  private bool _switchAxisHeldLastFrame = false;
   
+  [Header("Switching Effect")]
+  [SerializeField] private AnimationCurve _effectInCurve;
+  [SerializeField] private AnimationCurve _effectOutCurve;
+  [SerializeField] private float _endFov;
+  [SerializeField] private float _effectDuration;
+  [SerializeField] private bool _playingEffect;
+
   private void Start() {
-    foreach (Camera camera in Camera.allCameras) {
-      camera.enabled = false;
+    if (!IsInputAxisValid(_switchAxis)) {
+      Debug.LogError("ViewSwitcher: SwitchAxis is invalid! Please set it to a valid input axis in the Input Manager.");
     }
-    _currentPlayerCamera.enabled = true;
+    _currentObjectInhabiting.SwitchTo();
+    
   }
 
   private void Update() {
-    
+    _secondsSinceLastSwitch += Time.deltaTime;
+
+    CheckForNewSelectedObject();
+    UpdateInhabitInput();
+    InhabitIfInputDetected();
+  }
+
+  // -------------------------------------------------
+
+  private void CheckForNewSelectedObject() {
+    SwitchableObject targetedObject = RaycastForSwitchableObject();
+    if (targetedObject == null) {
+      if (_selectedSwitchableObject != null) _selectedSwitchableObject.Selected(false);
+      _selectedSwitchableObject = null;
+      return;  
+    } 
+    if (targetedObject == _selectedSwitchableObject) {
+      return;
+    }
+    else {
+      _selectedSwitchableObject = targetedObject;
+      _selectedSwitchableObject.Selected(true);
+    }
+  }
+  private void UpdateInhabitInput() {
+    if (Input.GetAxisRaw(_switchAxis) == 1) {
+      if (_switchAxisHeldLastFrame) {
+        _switchAxisDown = false;
+        return;
+      }
+      else {
+        _switchAxisDown = true;
+      }
+    }
+    else {
+      _switchAxisDown = false;
+    }
+  }
+  private void InhabitIfInputDetected() {
+    if (_selectedSwitchableObject == null) return;
+    if (_selectedSwitchableObject == _currentObjectInhabiting) {
+      Debug.LogWarning("ViewSwitcher: The SelectedSwitchableObject is the same as the currently inhabited object! Is the raycast intersecting with it's origin's collider?");
+      return;
+    }
+    if (_switchAxisDown && CanSwitch()) {
+      PlayFovEffectAndSwitch();
+    }
+  }
+  private void PlayFovEffectAndSwitch() {
+    if (_playingEffect) return;
+    else {
+      StartCoroutine(PlayFovEffectAndSwitchCoroutine());
+    }
+  }
+  private IEnumerator PlayFovEffectAndSwitchCoroutine() {
+    _playingEffect = true;
+    float timeElapsed = 0f;
+    float startFov = _currentObjectInhabiting._objectCameras[0].fieldOfView;
+    // Kept in memory in case the player looks away from object during effect
+    SwitchableObject selectedObject = _selectedSwitchableObject;
+    while (timeElapsed < _effectDuration / 2) {
+      _currentObjectInhabiting._objectCameras[0].fieldOfView = Mathf.Lerp(startFov, _endFov, _effectInCurve.Evaluate(timeElapsed / (_effectDuration / 2)));
+      yield return null;
+      timeElapsed += Time.deltaTime;
+      print(timeElapsed);
+    }
+    _currentObjectInhabiting._objectCameras[0].fieldOfView = _endFov;
+    SwitchToSelected(selectedObject);
+    timeElapsed = 0f;
+    while (timeElapsed < _effectDuration / 2) {
+      _currentObjectInhabiting._objectCameras[0].fieldOfView = Mathf.Lerp(startFov, _endFov, _effectOutCurve.Evaluate(timeElapsed / (_effectDuration / 2)));
+      yield return null;
+      timeElapsed += Time.deltaTime;
+      print(timeElapsed);
+    }
+    _currentObjectInhabiting._objectCameras[0].fieldOfView = startFov;
+    _playingEffect = false;
+  }
+  private void SwitchToSelected(SwitchableObject selectedObject) {
+    _secondsSinceLastSwitch = 0f;
+    selectedObject.SwitchTo();
+    _currentObjectInhabiting.SwitchAway();
+    _currentObjectInhabiting = selectedObject;
+    _selectedSwitchableObject = null;
+  }
+  private bool CanSwitch() {
+    if (_secondsSinceLastSwitch <= _effectDuration + _switchCooldown || _playingEffect) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  private SwitchableObject RaycastForSwitchableObject() {
+    Transform raycastOrigin = _currentObjectInhabiting._raycastOrigin;
+    Vector3 raycastOriginOffset = _currentObjectInhabiting._raycastOriginOffset;
+    float raycastDistance = _currentObjectInhabiting._raycastDistance;
+    LayerMask layerMask = _currentObjectInhabiting._layerMask;
+    bool showRaycast = _currentObjectInhabiting._showRaycast;
+
+    bool didHitCollider = Physics.Raycast(raycastOrigin.position + raycastOriginOffset, raycastOrigin.forward, out RaycastHit hit, raycastDistance, layerMask);
+    if (showRaycast) {
+      DrawDebugRaycast(raycastOrigin.position + raycastOriginOffset, raycastOrigin.forward, raycastDistance, didHitCollider ? Color.yellow : Color.green);
+    }
+    if (hit.collider == null) return null;
+    else return hit.collider.GetComponent<SwitchableObject>();
+  }
+  private bool IsInputAxisValid(string axisName) {
+    try {
+      Input.GetAxis(axisName);
+      return true;
+    }
+    catch (System.Exception e){
+      Debug.LogError(e);
+      return false;
+    }
+  }
+  private void DrawDebugRaycast(Vector3 origin, Vector3 direction, float distance, Color color) {
+    Debug.DrawLine(origin, (direction.normalized * distance) + origin, color, Time.deltaTime);
   }
 }
