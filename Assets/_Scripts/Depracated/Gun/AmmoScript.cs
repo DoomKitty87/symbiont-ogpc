026e70873dcdcd80b3679a0171cf8d85aeed4999
PhoenixFire <72724975+help-programming-is-hard@users.noreply.github.com>
2023-04-07 09:15:13 -0700

    working on ammo counter

--- Assets/_Scripts/Depracated/Gun/AmmoScript.cs
+++ Assets/_Scripts/Depracated/Gun/AmmoScript.cs
@@ -1,28 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class AmmoScript : MonoBehaviour 
+public class AmmoScript : MonoBehaviour
 {
+  // For display only
+  [SerializeField] private WeaponItem _currentWeaponItem;
 
-  [SerializeField] Color32 targetColor;
+  [Header("UI")]
+  [SerializeField] private Image _imageToChangeColor;
+  [SerializeField] private TextMeshProUGUI _currentAmmoText;
+  [SerializeField] private TextMeshProUGUI _maxAmmoText;
 
-  [HideInInspector] public int maxAmmo;
-  [HideInInspector] public int currAmmo;
-
+  [Header("Colors")]
+  [SerializeField] private Color32 _defaultColor;
+  [SerializeField] private Color32 _lowAmmoColor;
+  [Header("Values")]
+  // Needs to be public because GunController depends
+  [SerializeField] public int _maxAmmo;
+  [SerializeField] public int _currAmmo;
+  [Header("Easing")]
+  [SerializeField] private float _easeDuration;
+  [SerializeField] private AnimationCurve _easeCurve;
   private float colorCoefficient;
-  private Color32 defaultColor;
-  private Image image;
+  
 
   private void Start() {
-    image = GetComponent<Image>();
-    defaultColor = image.color;
+    _imageToChangeColor = GetComponent<Image>();
+    _defaultColor = _imageToChangeColor.color;
+    _currAmmo = 0;
+    _maxAmmo = 0;
   }
 
-  private void Update() {
-    if (currAmmo != 0 && maxAmmo != 0) {
+  // TODO: FIX THIS
+  public void UpdateForNewValues(WeaponItem weaponItem, int ammoCount) {
+    _currentWeaponItem = weaponItem;
+    StartCoroutine(TweenTextValue(_maxAmmoText, _maxAmmo, weaponItem.magSize, _easeDuration, 0));
+    _maxAmmo = weaponItem.magSize;
+    StartCoroutine(TweenTextValue(_currentAmmoText, _currAmmo, ammoCount, _easeDuration, 0));
+    _currAmmo = ammoCount;
+  }
+
+  public void DecrementAmmo() {
+    _currAmmo--;
+    UpdateColor();
+    _currentAmmoText.text = _currAmmo.ToString();
+  }
+
+  public void ResetAmmo() {
+    StartCoroutine(TweenTextValue(_currentAmmoText, _currAmmo, _maxAmmo, _easeDuration, 0));
+    _currAmmo = _maxAmmo;
+    UpdateColor();
+  }
+
+
+  private void UpdateColor() {
+    if (_currAmmo != 0 && _maxAmmo != 0) {
 			// If you aren't dividing by zero, exponentially raise colorCoefficient based on how small currAmmo is in respect to maxAmmo
-			colorCoefficient = ((maxAmmo/currAmmo) * (255 / maxAmmo));
+			colorCoefficient = ((_maxAmmo/_currAmmo) * (255 / _maxAmmo));
     }
-    image.color = Color.Lerp(defaultColor, targetColor, colorCoefficient / 255);
+    _imageToChangeColor.color = Color.Lerp(_defaultColor, _lowAmmoColor, colorCoefficient / 255);
+  }
+
+  private void UpdateText() {
+    
+  }
+
+  private IEnumerator TweenTextValue(TextMeshProUGUI text, float startValue, float targetValue, float duration, int decimalPlaces)
+  {
+    float timeElapsed = 0;
+    // float initSliderValue = slider.value;
+    while (timeElapsed < duration)
+    {
+      timeElapsed += Time.deltaTime;
+      float t = timeElapsed / duration;
+      t = _easeCurve.Evaluate(t);
+      text.text = Math.Round(Mathf.Lerp(startValue, targetValue, t), decimalPlaces).ToString();
+      yield return null;
+    }
+    text.text = targetValue.ToString();
   }
 }
