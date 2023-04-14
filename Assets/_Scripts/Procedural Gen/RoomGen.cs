db008bdff24a85a0cda9a7d6a570b6835d1036ac
DoomKitty87 <doomkitty887@gmail.com>
2023-04-14 12:21:07 -0700

    changed overlap checking

--- Assets/_Scripts/Procedural Gen/RoomGen.cs
+++ Assets/_Scripts/Procedural Gen/RoomGen.cs
@@ -9,6 +9,7 @@ public class RoomGen : MonoBehaviour
   [SerializeField] private GameObject[] genRooms;
   [SerializeField] private GameObject doorFiller;
   [SerializeField] private float roomSize;
+  [SerializeField] private LayerMask roomLayer;
 
   private List<GameObject> generatedRooms = new List<GameObject>();
 
@@ -29,19 +30,30 @@ public class RoomGen : MonoBehaviour
     int chosenDoor = Random.Range(0, lastRoom.transform.GetChild(1).childCount);
     int connectingDoor = Random.Range(0, instantiatedRoom.transform.GetChild(1).childCount);
       
-    instantiatedRoom.transform.position = lastRoom.transform.GetChild(1).GetChild(chosenDoor).position - instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).position;
-    while (Mathf.Abs((lastRoom.transform.position - instantiatedRoom.transform.position).magnitude) < roomSize) {
+    //instantiatedRoom.transform.position = lastRoom.transform.GetChild(1).GetChild(chosenDoor).position - instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).position;
+    //while (Mathf.Abs((lastRoom.transform.position - instantiatedRoom.transform.position).magnitude) < roomSize) {
+    //  instantiatedRoom.transform.RotateAround(lastRoom.transform.GetChild(1).GetChild(chosenDoor).position, Vector3.up, 180);
+    //}
+
+    Collider[] cols = Physics.OverlapSphere(instantiatedRoom.transform.position, instantiatedRoom.GetComponent<Renderer>.bounds.x / 2, roomLayer);
+
+    while (cols.Length > 1) {
       instantiatedRoom.transform.RotateAround(lastRoom.transform.GetChild(1).GetChild(chosenDoor).position, Vector3.up, 180);
+      cols = Physics.OverlapSphere(instantiatedRoom.transform.position, instantiatedRoom.GetComponent<Renderer>.bounds.x / 2, roomLayer);
     }
 
     loop:
       foreach (GameObject room in generatedRooms) {
-        if (Mathf.Abs(Vector3.Distance(room.transform.position, instantiatedRoom.transform.position)) < roomSize) {
+        cols = Physics.OverlapSphere(room.transform.position, room.GetComponent<Renderer>.bounds.x / 2, roomLayer);
+        if (cols.Length > 1) {
           chosenDoor = Random.Range(0, lastRoom.transform.GetChild(1).childCount);
           connectingDoor = Random.Range(0, instantiatedRoom.transform.GetChild(1).childCount);
           instantiatedRoom.transform.position = lastRoom.transform.GetChild(1).GetChild(chosenDoor).position - instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).position;
-          while (Mathf.Abs(Vector3.Distance(lastRoom.transform.position, instantiatedRoom.transform.position)) < roomSize) {
+          cols = Physics.OverlapSphere(instantiatedRoom.transform.position, instantiatedRoom.GetComponent<Renderer>.bounds.x / 2, roomLayer);
+
+          while (cols.Length > 1) {
             instantiatedRoom.transform.RotateAround(lastRoom.transform.GetChild(1).GetChild(chosenDoor).position, Vector3.up, 180);
+            cols = Physics.OverlapSphere(instantiatedRoom.transform.position, instantiatedRoom.GetComponent<Renderer>.bounds.x / 2, roomLayer);
           }
           goto loop;
         }
@@ -62,44 +74,54 @@ public class RoomGen : MonoBehaviour
 
   private void GenerateDungeon(int rooms) {
 
-  GameObject _roomChoice = genRooms[Random.Range(0, genRooms.Length)];
-  GameObject _instantiatedRoom = Instantiate(_roomChoice, new Vector3(0, 0, 0), Quaternion.identity, transform);
-  lastRoom = _instantiatedRoom;
+    GameObject _roomChoice = genRooms[Random.Range(0, genRooms.Length)];
+    GameObject _instantiatedRoom = Instantiate(_roomChoice, new Vector3(0, 0, 0), Quaternion.identity, transform);
+    lastRoom = _instantiatedRoom;
 
-  for (int i = 0; i < rooms - 1; i++) {
-    GameObject roomChoice = genRooms[Random.Range(0, genRooms.Length)];
-    GameObject instantiatedRoom = Instantiate(roomChoice, new Vector3(0, 0, 0), Quaternion.identity, transform);
+    for (int i = 0; i < rooms - 1; i++) {
+      GameObject roomChoice = genRooms[Random.Range(0, genRooms.Length)];
+      GameObject instantiatedRoom = Instantiate(roomChoice, new Vector3(0, 0, 0), Quaternion.identity, transform);
 
-    int chosenDoor = Random.Range(0, lastRoom.transform.GetChild(1).childCount);
-    int connectingDoor = Random.Range(0, instantiatedRoom.transform.GetChild(1).childCount);
+      int chosenDoor = Random.Range(0, lastRoom.transform.GetChild(1).childCount);
+      int connectingDoor = Random.Range(0, instantiatedRoom.transform.GetChild(1).childCount);
       
-    instantiatedRoom.transform.position = lastRoom.transform.GetChild(1).GetChild(chosenDoor).position - instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).position;
-    while (Mathf.Abs((lastRoom.transform.position - instantiatedRoom.transform.position).magnitude) < roomSize) {
-      instantiatedRoom.transform.RotateAround(lastRoom.transform.GetChild(1).GetChild(chosenDoor).position, Vector3.up, 180);
-    }
+      instantiatedRoom.transform.position = lastRoom.transform.GetChild(1).GetChild(chosenDoor).position - instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).position;
+      //while (Mathf.Abs((lastRoom.transform.position - instantiatedRoom.transform.position).magnitude) < roomSize) {
+      //  instantiatedRoom.transform.RotateAround(lastRoom.transform.GetChild(1).GetChild(chosenDoor).position, Vector3.up, 180);
+      //}
 
-    loop:
-      foreach (GameObject room in generatedRooms) {
-        if (Mathf.Abs(Vector3.Distance(room.transform.position, instantiatedRoom.transform.position)) < roomSize) {
-          chosenDoor = Random.Range(0, lastRoom.transform.GetChild(1).childCount);
-          connectingDoor = Random.Range(0, instantiatedRoom.transform.GetChild(1).childCount);
+      Collider[] cols = Physics.OverlapSphere(instantiatedRoom.transform.position, instantiatedRoom.GetComponent<Renderer>.bounds.x / 2, roomLayer);
 
-          instantiatedRoom.transform.position = lastRoom.transform.GetChild(1).GetChild(chosenDoor).position - instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).position;
-          while (Mathf.Abs(Vector3.Distance(lastRoom.transform.position, instantiatedRoom.transform.position)) < roomSize) {
-            instantiatedRoom.transform.RotateAround(lastRoom.transform.GetChild(1).GetChild(chosenDoor).position, Vector3.up, 180);
-          }
-          goto loop;
-        }
+      while (cols.Length > 1) {
+        instantiatedRoom.transform.RotateAround(lastRoom.transform.GetChild(1).GetChild(chosenDoor).position, Vector3.up, 180);
+        cols = Physics.OverlapSphere(instantiatedRoom.transform.position, instantiatedRoom.GetComponent<Renderer>.bounds.x / 2, roomLayer);
       }
 
-    Destroy(instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).gameObject);
-    generatedRooms.Add(lastRoom);
-    for (int door = 0; door < lastRoom.transform.GetChild(1).childCount; ++door) {
-      lastRoom.transform.GetChild(1).GetChild(door).gameObject.SetActive(true);
-      //Will cause undesired behavior but I'll deal with that in a bit
-    }
+      loop:
+        foreach (GameObject room in generatedRooms) {
+          cols = Physics.OverlapSphere(room.transform.position, room.GetComponent<Renderer>.bounds.x / 2, roomLayer);
+          if (cols.Length > 1) {
+            chosenDoor = Random.Range(0, lastRoom.transform.GetChild(1).childCount);
+            connectingDoor = Random.Range(0, instantiatedRoom.transform.GetChild(1).childCount);
+            instantiatedRoom.transform.position = lastRoom.transform.GetChild(1).GetChild(chosenDoor).position - instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).position;
+            cols = Physics.OverlapSphere(instantiatedRoom.transform.position, instantiatedRoom.GetComponent<Renderer>.bounds.x / 2, roomLayer);
 
-    lastRoom = instantiatedRoom;
+            while (cols.Length > 1) {
+              instantiatedRoom.transform.RotateAround(lastRoom.transform.GetChild(1).GetChild(chosenDoor).position, Vector3.up, 180);
+              cols = Physics.OverlapSphere(instantiatedRoom.transform.position, instantiatedRoom.GetComponent<Renderer>.bounds.x / 2, roomLayer);
+            }
+            goto loop;
+          }
+        }
+
+      Destroy(instantiatedRoom.transform.GetChild(1).GetChild(connectingDoor).gameObject);
+      generatedRooms.Add(lastRoom);
+      for (int door = 0; door < lastRoom.transform.GetChild(1).childCount; ++door) {
+        if (door == chosenDoor) continue;
+        lastRoom.transform.GetChild(1).GetChild(door).gameObject.SetActive(true);
+      }
+
+      lastRoom = instantiatedRoom;
     }
   }
 }
