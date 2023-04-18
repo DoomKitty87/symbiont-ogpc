ceca0f88004c667083181ca80747203e9d7a7d71
Elliot <93895160+2Elliot@users.noreply.github.com>
2023-04-18 00:27:03 -0700

    Doors are more fixed i guess

--- Assets/DoorCameraFollow.cs
+++ Assets/DoorCameraFollow.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorCameraFollow : MonoBehaviour
 {
-  public Transform door;
-  public Transform otherDoor;
+  [HideInInspector] public Transform door;
+  [HideInInspector] public Transform otherDoor;
 
-  private Transform playerCamera;
+  public Transform playerCamera;
 
   private void Awake() {
     playerCamera = Camera.main.transform;
@@ -17,19 +15,18 @@ public class DoorCameraFollow : MonoBehaviour
   private void Update() {
     if (otherDoor) {
       Vector3 playerOffsetFromDoor = otherDoor.position - playerCamera.position;
-      transform.localPosition = door.position - playerOffsetFromDoor;
-      // ((playerOffsetFromDoor.normalized + new Vector3(door.rotation.eulerAngles.x, door.rotation.eulerAngles.y, door.rotation.eulerAngles.z)).normalized
-      // * playerOffsetFromDoor.magnitude);
-      // Debug.Log(new Vector3(door.rotation.eulerAngles.x, door.rotation.eulerAngles.y, door.rotation.eulerAngles.z).normalized + playerOffsetFromDoor.normalized);
-      // Debug.Log(playerOffsetFromDoor.normalized);
-      // ((door.rotation.eulerAngles.normalized + playerOffsetFromDoor.normalized).normalized * playerOffsetFromDoor.magnitude);
+      transform.localPosition = playerOffsetFromDoor;
 
-      Quaternion fixedRotation = Quaternion.Euler(door.eulerAngles.x- 180, door.eulerAngles.y, door.eulerAngles.z);
+      // Quaternion fixedRotation = Quaternion.Euler(door.eulerAngles.x, door.eulerAngles.y, door.eulerAngles.z);
 
-      float angularDifferenceBetweenTwoDoors = Quaternion.Angle(fixedRotation, otherDoor.rotation);
-      Quaternion doorRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenTwoDoors, Vector3.up);
-      Vector3 newCameraDirection = doorRotationDifference * -playerCamera.forward;
-      transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+      //float angularDifferenceBetweenTwoDoors = Quaternion.Angle(fixedRotation, otherDoor.rotation);
+      //Quaternion doorRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenTwoDoors, Vector3.forward);
+      //Vector3 newCameraDirection = doorRotationDifference * -playerCamera.forward;
+      //transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+      Debug.Log(playerCamera.transform.position);
+
+      transform.rotation = playerCamera.transform.rotation * 
+        Quaternion.Euler(door.rotation.eulerAngles.x, door.rotation.eulerAngles.y + 180, door.rotation.eulerAngles.z);
     }
   }
 }
