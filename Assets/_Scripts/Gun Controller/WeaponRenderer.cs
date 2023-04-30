522103ac4a24073b1ed9dbe5896619cda9ccf9d4
DoomKitty87 <doomkitty887@gmail.com>
2023-04-30 12:14:00 -0700

    done fixing errors and new color stuff

--- Assets/_Scripts/Gun Controller/WeaponRenderer.cs
+++ Assets/_Scripts/Gun Controller/WeaponRenderer.cs
@@ -49,7 +49,6 @@ public class WeaponRenderer : MonoBehaviour
   public void UpdateForNewValues(WeaponItem weaponItem, int ammoCount) {
     // print("WeaponRenderer: Updating for new values.");
     ShowNewWeapon(weaponItem);
-    print("trying to create weapon");
     gameObject.GetComponent<WeaponEffects>().UpdateForNewValues(weaponItem, ammoCount);
   }
   public GameObject GetWeaponInstance(WeaponItem weaponItem) {
@@ -68,7 +67,6 @@ public class WeaponRenderer : MonoBehaviour
     }
     else {
       InstantiateWeapon(weaponItem);
-      print("instantiated weapon");
       // The spawnedWeapon has just been added to the list, so it's index would be -1 from the list.Count
       ViewSpawnedWeaponAtIndex(_spawnedWeapons.Count - 1);
     }
