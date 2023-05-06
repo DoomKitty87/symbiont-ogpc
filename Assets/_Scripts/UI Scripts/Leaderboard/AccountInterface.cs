6c1b625e1fd91bef8a9dd8911adcb9be151c59bc
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2023-05-06 08:31:06 -0700

    Moved leaderboard scripts into UI Scripts, starting integration with menu

---
+++ Assets/_Scripts/UI Scripts/Leaderboard/AccountInterface.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AccountInterface : MonoBehaviour
+{
+
+  private LoginConnect _loginManager;
+  [SerializeField] private TMP_InputField _loginUsername, _loginPassword, _registerUsername, _registerPassword, _registerConfirmPassword;
+  //[SerializeField] private TMP_InputField deleteUser, deletePass, deleteConfirmPass, regEmail
+  [SerializeField] private TextMeshProUGUI _activeAccountDisplay;
+
+  
+
+  void Start() {
+    _loginManager = GameObject.FindGameObjectWithTag("ConnectionManager").GetComponent<LoginConnect>();
+  }
+
+  public void OnClickLogin() {
+    string name = _loginUsername.text;
+    string password = _loginPassword.text;
+    StartCoroutine(_loginManager.Login(name, password));
+    if (_loginManager.IsLoggedIn()) _activeAccountDisplay.text = _loginManager.GetActiveAccountName();
+  }
+
+  public void OnClickLogout() {
+    _loginManager.Logout();
+    _activeAccountDisplay.text = "Not logged in.";
+  }
+
+  /*
+  public void OnClickDelete() {
+    string name = deleteUser.text;
+    string password = deletePass.text;
+    string confirmpassword = deleteConfirmPass.text;
+    string result = loginManager.DeleteAccount(name, password, confirmpassword);
+    if (result == "Passwords did not match.") print(result);
+  }
+  */
+
+  public void OnClickRegister() {
+    string name = _registerUsername.text;
+    string email = "no_email";
+    string password = _registerPassword.text;
+    string confirmpassword = _registerConfirmPassword.text;
+    string result = _loginManager.Register(email, name, password, confirmpassword);
+    if (result == "Passwords did not match.") print(result);
+  }
+}
