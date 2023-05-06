6eee850459cd94c2c875152abbfd9fb6ec5cc73a
DoomKitty87 <doomkitty887@gmail.com>
2023-05-05 17:45:36 -0700

    email and delete removed

--- Assets/_Scripts/Leaderboard/AccountInterface.cs
+++ Assets/_Scripts/Leaderboard/AccountInterface.cs
@@ -7,12 +7,12 @@ public class AccountInterface : MonoBehaviour
 {
 
   private LoginConnect loginManager;
-  [SerializeField] private TMP_InputField loginUser, loginPass, deleteUser, deletePass, deleteConfirmPass, regUser, regEmail, regPass, regConfirmPass;
+  [SerializeField] private TMP_InputField loginUser, loginPass, regUser, regPass, regConfirmPass;
+  //[SerializeField] private TMP_InputField deleteUser, deletePass, deleteConfirmPass, regEmail
   [SerializeField] private TextMeshProUGUI activeAccount;
 
   void Start() {
     loginManager = GameObject.FindGameObjectWithTag("ConnectionManager").GetComponent<LoginConnect>();
-
   }
 
   public void OnClickLogin() {
@@ -27,6 +27,7 @@ public class AccountInterface : MonoBehaviour
     activeAccount.text = "Not logged in.";
   }
 
+  /*
   public void OnClickDelete() {
     string name = deleteUser.text;
     string password = deletePass.text;
@@ -34,10 +35,11 @@ public class AccountInterface : MonoBehaviour
     string result = loginManager.DeleteAccount(name, password, confirmpassword);
     if (result == "Passwords did not match.") print(result);
   }
+  */
 
   public void OnClickRegister() {
     string name = regUser.text;
-    string email = regEmail.text;
+    string email = "no_email";
     string password = regPass.text;
     string confirmpassword = regConfirmPass.text;
     string result = loginManager.Register(email, name, password, confirmpassword);
