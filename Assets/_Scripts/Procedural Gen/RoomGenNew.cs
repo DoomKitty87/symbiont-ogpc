e9e96ad7d450c9d63b1d2441390048399cf96f47
Elliot <93895160+2Elliot@users.noreply.github.com>
2023-05-08 22:07:44 -0700

    door bag system

--- Assets/_Scripts/Procedural Gen/RoomGenNew.cs
+++ Assets/_Scripts/Procedural Gen/RoomGenNew.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomGenNew : MonoBehaviour
@@ -7,32 +8,36 @@ public class RoomGenNew : MonoBehaviour
 	// Enemies
   // Doors
   // Components
+  // Cameras
 
-  private GameObject[] randomRooms;
-  private GameObject[] startingRooms;
-  private GameObject[] endingRooms;
+  private GameObject[] _randomRooms;
+  private GameObject[] _startingRooms;
+  private GameObject[] _endingRooms;
+
+  public List<string> _roomBag;
+  public List<string> _roomBagEmpty;
 
   [SerializeField] private int _numberOfRoomsPerFloor;
 
 	private Vector3 _nextCoordinates = new(-100, 0, -100);
   private bool _parody; // Used for things that require odd or even (Basically don't worry about it)
-  private int roomsGenerated = 0;
+  private int _roomsGenerated = 0;
 
   [HideInInspector] public GameObject _currentRoom;
   [HideInInspector] public GameObject _previousRoom;
-  [HideInInspector] public GameObject startingRoom;
+  [HideInInspector] public GameObject _startingRoom;
 
 	private void Awake() {
-    randomRooms = GameObject.FindGameObjectWithTag("Persistent").GetComponent<FloorManager>().GetCurrentFloorRooms();
-    startingRooms = GameObject.FindGameObjectWithTag("Persistent").GetComponent<FloorManager>().GetCurrentFloorStartRooms();
-    endingRooms = GameObject.FindGameObjectWithTag("Persistent").GetComponent<FloorManager>().GetCurrentFloorEndRooms();
+    _randomRooms = GameObject.FindGameObjectWithTag("Persistent").GetComponent<FloorManager>().GetCurrentFloorRooms();
+    _startingRooms = GameObject.FindGameObjectWithTag("Persistent").GetComponent<FloorManager>().GetCurrentFloorStartRooms();
+    _endingRooms = GameObject.FindGameObjectWithTag("Persistent").GetComponent<FloorManager>().GetCurrentFloorEndRooms();
 
-    startingRoom = Instantiate(startingRooms[Random.Range(0, startingRooms.Length)], Vector3.zero, Quaternion.identity, transform);
+    _startingRoom = Instantiate(_startingRooms[Random.Range(0, _startingRooms.Length)], Vector3.zero, Quaternion.identity, transform);
 	  // Assigns the first room as the current object inhabited
 	  GameObject.FindWithTag("PlayerHolder").GetComponent<ViewSwitcher>()._currentObjectInhabiting = 
-      startingRoom.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<SwitchableObject>();
-    roomsGenerated++;
-    _currentRoom = startingRoom;
+      _startingRoom.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<SwitchableObject>();
+    _roomsGenerated++;
+    _currentRoom = _startingRoom;
 	}
 
   public void CreateNewRoom() {
@@ -46,12 +51,26 @@ public class RoomGenNew : MonoBehaviour
     // Creates new room
     if (_previousRoom != null) Destroy(_previousRoom);
     _previousRoom = _currentRoom;
-    if (roomsGenerated == _numberOfRoomsPerFloor - 1) {
-      _currentRoom = Instantiate(endingRooms[Random.Range(0, endingRooms.Length)], _nextCoordinates, Quaternion.identity, transform);
+    if (_roomsGenerated == _numberOfRoomsPerFloor - 1) {
+      _currentRoom = Instantiate(_endingRooms[Random.Range(0, _endingRooms.Length)], _nextCoordinates, Quaternion.identity, transform);
       _numberOfRoomsPerFloor = -1; // Effectively ending room generation
       return;
     }
-    _currentRoom = Instantiate(randomRooms[Random.Range(0, randomRooms.Length)], _nextCoordinates, Quaternion.identity, transform);
+
+    // If the room bag is empty, refill it and empty the empty bag
+    if (_roomBag.Count == 0) {
+      _roomBagEmpty.Clear();
+      foreach (GameObject room in _randomRooms) {
+        _roomBag.Add(room.name);
+      }
+    }
+
+    // Deals with creating new rooms
+    GameObject roomToAdd = Instantiate(GetRoomWithName(_roomBag[Random.Range(0, _roomBag.Count)], _randomRooms), _nextCoordinates, Quaternion.identity, transform);
+
+    _currentRoom = roomToAdd;
+    _roomBagEmpty.Add(roomToAdd.name.Remove(roomToAdd.name.Length - 7));
+    _roomBag.Remove(roomToAdd.name.Remove(roomToAdd.name.Length - 7));
     
     if (_parody) {
       _parody ^= true;
@@ -60,6 +79,25 @@ public class RoomGenNew : MonoBehaviour
       _parody ^= true;
       _nextCoordinates.z *= -1;
     }
-    roomsGenerated++;
+    _roomsGenerated++;
   }
+
+  private GameObject GetRoomWithName(string s, GameObject[] roomArray) {
+    foreach(GameObject gameObject in roomArray) {
+      if (gameObject.name == s) {
+        return gameObject;
+      }
+    }
+    Debug.LogError("No gameObject with name " + s + " found in " + roomArray + " array.");
+    return null;
+  }
+
+  /*
+  private string RemoveCharacterFromEndOfString(string startingString, int numberOfCharacterToRemove) {
+    string newString = "";
+    for (int i = startingString.Length; i < startingString.Length - numberOfCharacterToRemove; i++) {
+			newString += startingString[i];
+		}
+  }
+  */
 }
