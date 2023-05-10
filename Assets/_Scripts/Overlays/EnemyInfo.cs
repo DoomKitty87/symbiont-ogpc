753894143a49bdfda0b304b04fb3a3fb76e844f7
DoomKitty87 <doomkitty887@gmail.com>
2023-05-09 22:07:31 -0700

    further work on enemy titles

--- Assets/_Scripts/Overlays/EnemyInfo.cs
+++ Assets/_Scripts/Overlays/EnemyInfo.cs
@@ -11,6 +11,11 @@ public class EnemyInfo : MonoBehaviour
   [SerializeField] private LayerMask enemyLayer;
   [SerializeField] private GameObject enemyOverlayPrefab;
   [SerializeField] private GameObject enemyCanvas;
+  [SerializeField] private float _minScale, _maxScale, _maxRange;
+
+  private string[] _models = {"DXRM", "FNMT", "CRLX", "ZKBL"};
+  private List<string[]> _robotData = new List<string[]>();
+  private List<GameObject> _robotsSeen = new List<GameObject>();
 
   private void Start() {
     cam = GetComponent<Camera>();
@@ -21,7 +26,8 @@ public class EnemyInfo : MonoBehaviour
     while (modelNo.Length < 3) {
       modelNo = "0" + modelNo;
     }
-    return new string[] {modelNo};
+    string modelType = _models[Random.Range(0, _models.Length)];
+    return new string[] {modelNo, modelType};
   }
 
   public void SwitchedAway() {
@@ -34,7 +40,7 @@ public class EnemyInfo : MonoBehaviour
     for (int i = 0; i < enemyCanvas.transform.childCount; i++) {
       Destroy(enemyCanvas.transform.GetChild(i).gameObject);
     }
-    Collider[] cols = Physics.OverlapSphere(transform.position, 50f, enemyLayer);
+    Collider[] cols = Physics.OverlapSphere(transform.position, _maxRange, enemyLayer);
     foreach (Collider col in cols) {
       Vector3 screenPos = cam.WorldToScreenPoint(col.gameObject.transform.position);
       if (col.gameObject != GameObject.FindGameObjectWithTag("PlayerHolder").GetComponent<ViewSwitcher>()._currentObjectInhabiting.gameObject) {
@@ -43,10 +49,18 @@ public class EnemyInfo : MonoBehaviour
         Physics.Linecast(transform.position, col.gameObject.transform.position, out hit);
         if (hit.collider != col) continue;
         GameObject tmp = Instantiate(enemyOverlayPrefab, Vector3.zero, Quaternion.identity, enemyCanvas.transform);
-        tmp.transform.GetChild(0).position = cam.WorldToScreenPoint(col.gameObject.transform.position + new Vector3(0, GameObject.FindGameObjectWithTag("PlayerHolder").GetComponent<ViewSwitcher>()._currentObjectInhabiting.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.size.y / 2, 0));
-        tmp.transform.GetChild(1).position = cam.WorldToScreenPoint(col.gameObject.transform.position - new Vector3(0, GameObject.FindGameObjectWithTag("PlayerHolder").GetComponent<ViewSwitcher>()._currentObjectInhabiting.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.size.y / 2, 0));     
+        tmp.transform.GetChild(0).position = cam.WorldToScreenPoint(col.gameObject.transform.position + new Vector3(0, GameObject.FindGameObjectWithTag("PlayerHolder").GetComponent<ViewSwitcher>()._currentObjectInhabiting.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.size.y / 1.2f, 0));
+        tmp.transform.GetChild(1).position = cam.WorldToScreenPoint(col.gameObject.transform.position - new Vector3(0, GameObject.FindGameObjectWithTag("PlayerHolder").GetComponent<ViewSwitcher>()._currentObjectInhabiting.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.size.y / 1.2f, 0));     
+        if (!_robotsSeen.Contains(col.gameObject)) {
+          _robotsSeen.Add(col.gameObject);
+          _robotData.Add(GenRobotData());
+        }
+        
+        tmp.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = "MODEL " + _robotData[_robotsSeen.IndexOf(col.gameObject)][1] + "- #" + _robotData[_robotsSeen.IndexOf(col.gameObject)][0];
+        tmp.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = "STATE: " + "ALERTED";
 
-        tmp.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = "Model" + Random.Range(0, 999);
+        tmp.transform.GetChild(2).position = cam.WorldToScreenPoint(col.gameObject.transform.position + new Vector3(0, GameObject.FindGameObjectWithTag("PlayerHolder").GetComponent<ViewSwitcher>()._currentObjectInhabiting.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.size.y / 2f, 0)) + new Vector3(20 * -GameObject.FindGameObjectWithTag("PlayerHolder").GetComponent<ViewSwitcher>()._currentObjectInhabiting.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.size.x, 0, 0);
+        tmp.transform.GetChild(2).localScale *= Mathf.Lerp(_maxScale, _minScale, Vector3.Distance(transform.position, col.gameObject.transform.position) / _maxRange);
       }
     }
   }
