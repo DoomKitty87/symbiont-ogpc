06b429d2af4b0be2ee8018d90a8cfc397746fa46
DoomKitty87 <doomkitty887@gmail.com>
2023-05-10 22:03:55 -0700

    wip on ui

---
+++ Assets/_Scripts/UI Scripts/InGameUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour
+{
+
+  [SerializeField] private GameObject _healthHolder, _ammoHolder;
+
+  public void UpdateHealth() {
+    return;
+  }
+}
