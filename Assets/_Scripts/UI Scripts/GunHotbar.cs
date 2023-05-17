3300c5832f6e0ec16f10af120fbcd532b80466b1
DoomKitty87 <doomkitty887@gmail.com>
2023-05-16 19:51:12 -0700

    working on hotbar stuff

--- Assets/_Scripts/UI Scripts/GunHotbar.cs
+++ Assets/_Scripts/UI Scripts/GunHotbar.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GunHotbar : MonoBehaviour
 {
 
   [SerializeField] private GameObject _gunSlot1, _gunSlot2;
+  [SerializeField] private Sprite _emptySprite;
 
   public void LoadInfoForGuns(WeaponItem[] weapons, WeaponItem weapon) {
     if (weapons.Length == 2) {
-      _gunSlot1.gameObject.GetComponent<TextMeshProUGUI>().text = weapons[0].name;
-      _gunSlot2.gameObject.GetComponent<TextMeshProUGUI>().text = weapons[1].name;
+      _gunSlot1.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = weapons[0].name.ToUpper();
+      _gunSlot2.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = weapons[1].name.ToUpper();
+      _gunSlot1.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = weapons[0].sprite;
+      _gunSlot2.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = weapons[0].sprite;
     }
     else {
-      _gunSlot1.GetComponent<TextMeshProUGUI>().text = weapons[0].name;
-      _gunSlot2.GetComponent<TextMeshProUGUI>().text = "NULL WEAPON";
+      _gunSlot1.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = weapons[0].name.ToUpper();
+      _gunSlot2.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = "NULL WEAPON";
+      _gunSlot1.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = weapons[0].sprite;
+      _gunSlot2.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = _emptySprite;
     }
   }
 
