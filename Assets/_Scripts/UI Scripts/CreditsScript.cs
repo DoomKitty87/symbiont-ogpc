701a8ba83c21b8ca99876584ddd3880e29dee7b9
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2023-05-16 23:11:18 -0700

    Fixed credits and options right arrow

--- Assets/_Scripts/UI Scripts/CreditsScript.cs
+++ Assets/_Scripts/UI Scripts/CreditsScript.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class CreditsScript : MonoBehaviour
 {
 
-  [SerializeField] private GameObject _creditsContent;
+  [SerializeField] private RectTransform _creditsContent;
   [SerializeField] private float _creditsScrollSpeed;
+  [SerializeField] private float _creditsScrollDistance;
+
+  private Vector3 _startPosition;
+
+  private void Start() {
+    _startPosition = _creditsContent.transform.localPosition;
+  }
 
   public void StartCredits() {
-    StopCoroutine("StartCreditsCoroutine");
+    StopCoroutine(StartCreditsCoroutine());
     StartCoroutine(StartCreditsCoroutine());
   }
 
   private IEnumerator StartCreditsCoroutine() {
-    _creditsContent.transform.localPosition = new Vector3(0, -Screen.height / 2f, 0);
-    while (_creditsContent.transform.GetChild(_creditsContent.transform.childCount - 1).position.y < Screen.height / 2f) {
+    _creditsContent.transform.localPosition = _startPosition;
+    while (_startPosition.y - _creditsContent.transform.localPosition.y < _creditsScrollDistance) {
       _creditsContent.transform.localPosition += new Vector3(0, _creditsScrollSpeed * Time.deltaTime, 0);
       yield return null;
     }
