e539eb6fd49ffdab971b0a9427b206af4222c9c9
DoomKitty87 <53231162+DoomKitty87@users.noreply.github.com>
2023-05-19 16:05:21 -0700

    fixed lb bug

--- Assets/_Scripts/UI Scripts/Leaderboard/AccountInterface.cs
+++ Assets/_Scripts/UI Scripts/Leaderboard/AccountInterface.cs
@@ -72,7 +72,10 @@ public class AccountInterface : MonoBehaviour
     _logOutButton.onClick.AddListener(OnClickLogout);
     _registerButton.onClick.AddListener(OnClickRegister);
     _deleteButton.onClick.AddListener(OnClickDelete);
-    if (_loginManager.IsLoggedIn()) _onLoginSuccess?.Invoke();
+    if (_loginManager.IsLoggedIn()) {
+      _activeAccountDisplayText.text = _loginManager.GetActiveAccountName();
+      _onLoginSuccess?.Invoke();
+    }
   }
 
   // For all account functions: OnClick will check for basics like password confirmation and empty fields.
