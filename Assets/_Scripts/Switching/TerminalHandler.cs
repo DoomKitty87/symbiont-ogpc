903c2b7e53b1484241579b5b2662853f0c15c94b
DoomKitty87 <doomkitty887@gmail.com>
2023-05-18 20:11:50 -0700

    terminal now does stuff

--- Assets/_Scripts/Switching/TerminalHandler.cs
+++ Assets/_Scripts/Switching/TerminalHandler.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class TerminalHandler : MonoBehaviour
 {
+  
+  private GameObject room;
+
+  public void Start() {
+    while (!room.CompareTag("Room")) {
+      room = room.transform.parent.gameObject;
+    }
+  }
+
+  public void Update() {
+    if (room.transform.GetChild(0).childCount > 2) {
+      gameObject.tag = "DisabledSwitchable";
+    }
+    else gameObject.tag = "SwitchableObject";
+  }
+
   public void TriggerFloorSwitch() {
     GameObject.FindGameObjectWithTag("Persistent").GetComponent<FloorManager>().ClearedFloor();
   }
