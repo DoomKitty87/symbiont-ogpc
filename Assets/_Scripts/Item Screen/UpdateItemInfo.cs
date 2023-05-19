7dd59c8a20c60f4807446f274e343fc8fc102d69
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2023-05-19 14:51:54 -0700

    added two more icones

---
+++ Assets/_Scripts/Item Screen/UpdateItemInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateItemInfo : MonoBehaviour
+{
+
+    [SerializeField] private PlayerItems _playerItems;
+
+    private void UpdateRandomItems() {
+        PlayerItem[] chosenItems = _playerItems.
+    }
+}
