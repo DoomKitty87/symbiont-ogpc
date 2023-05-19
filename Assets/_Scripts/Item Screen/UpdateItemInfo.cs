f9c4c0b2524fb9ffe3c01808e3798a02c4b73e44
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2023-05-19 15:13:28 -0700

    item invo updaterator?

--- Assets/_Scripts/Item Screen/UpdateItemInfo.cs
+++ Assets/_Scripts/Item Screen/UpdateItemInfo.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpdateItemInfo : MonoBehaviour
 {
 
     [SerializeField] private PlayerItems _playerItems;
+    [SerializeField] private GameObject _slotParent;
+    // [SerializeField] private ItemSelection _itemSelection;
 
-    private void UpdateRandomItems() {
-        PlayerItem[] chosenItems = _playerItems.
+
+    private void UpdateInventory() {
+        PlayerItem[] inventory = _playerItems.GetPlayerItems();
+        for (int i = 0; i < 5; i++) {
+            _slotParent.transform.GetChild(i).GetComponent<Image>().sprite = inventory[0].item.icon;
+        }
     }
 }
