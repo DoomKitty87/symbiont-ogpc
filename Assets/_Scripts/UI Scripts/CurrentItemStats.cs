fdbcda8de82b7b1e85cba0329a2c172e5e91e8ce
DoomKitty87 <53231162+DoomKitty87@users.noreply.github.com>
2023-05-19 18:10:17 -0700

    finished lerping for matthews backend script

--- Assets/_Scripts/UI Scripts/CurrentItemStats.cs
+++ Assets/_Scripts/UI Scripts/CurrentItemStats.cs
@@ -7,14 +7,41 @@ public class CurrentItemStats : MonoBehaviour
 {
 
   [SerializeField] private Image damageMult, damageOnLeave, maxHealthIncrease, healOnTeleport, dps, teleportSpeed;
+  [SerializeField] private AnimationCurve _easeCurve;
 
   public void RefreshItems() {
+    StopAllCoroutines();
+    StartCoroutine(LerpItemValues());
+  }
+
+  private IEnumerator LerpItemValues() {
     PlayerItems playerInv = GameObject.FindGameObjectWithTag("Persistent").GetComponent<PlayerItems>();
-    damageMult.fillAmount = playerInv.GetDamageMult() / 3;
-    damageOnLeave.fillAmount = playerInv.GetLeaveDamage() / 50f;
-    maxHealthIncrease.fillAmount = playerInv.GetMaxHealthIncrease() / 100f;
-    healOnTeleport.fillAmount = playerInv.GetHealOnTeleport() / 50f;
-    dps.fillAmount = playerInv.GetDPS() / 5f;
-    teleportSpeed.fillAmount = 1f / playerInv.GetTeleportSpeed() / 3f;
+    float dmgm = playerInv.GetDamageMult() / 3;
+    float dmgl = playerInv.GetLeaveDamage() / 50f;
+    float mxhlt = playerInv.GetMaxHealthIncrease() / 100f;
+    float hlontp = playerInv.GetHealOnTeleport() / 50f;
+    float dpsv = playerInv.GetDPS() / 5f;
+    float tpspd = 1f / playerInv.GetTeleportSpeed() / 3f;
+
+    float initdmgm = damageMult.fillAmount;
+    float initdmgl = damageOnLeave.fillAmount;
+    float initmxhlt = maxHealthIncrease.fillAmount;
+    float inithlontp = healOnTeleport.fillAmount;
+    float initdpsv = dps.fillAmount;
+    float inittpspd = teleportSpeed.fillAmount;
+
+    float duration = 0.3f;
+
+    float timeElapsed = 0;
+    while (timeElapsed <= duration) {
+      damageMult.fillAmount = Mathf.Lerp(initdmgm, dmgm, _easeCurve.Evaluate(timeElapsed / duration));
+      damageOnLeave.fillAmount = Mathf.Lerp(initdmgl, dmgl, _easeCurve.Evaluate(timeElapsed / duration));
+      maxHealthIncrease.fillAmount = Mathf.Lerp(initmxhlt, mxhlt, _easeCurve.Evaluate(timeElapsed / duration));
+      healOnTeleport.fillAmount = Mathf.Lerp(inithlontp, hlontp, _easeCurve.Evaluate(timeElapsed / duration));
+      dps.fillAmount = Mathf.Lerp(initdpsv, dpsv, _easeCurve.Evaluate(timeElapsed / duration));
+      teleportSpeed.fillAmount = Mathf.Lerp(inittpspd, tpspd, _easeCurve.Evaluate(timeElapsed / duration));
+      timeElapsed += Time.deltaTime;
+      yield return null;
+    }
   }
 }
